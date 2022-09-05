8a5c07f790797b35aac8fd45617dd5f187a1bd18
Abhishek001konni <Abhishek001konni@gmail.com>
2022-09-05 10:18:45 +0530

    miatoll: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,14 +2,14 @@ rm -rf hardware/xiaomi
 git clone --depth=1 https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-13.0 hardware/xiaomi
 
 # Device Common Tree
-git clone https://github.com/ProjectElixir-Devices/device_xiaomi-sm6250-common.git -b 12.1  device/xiaomi/sm6250-common
+git clone https://github.com/ProjectElixir-Devices/device_xiaomi-sm6250-common.git -b 13  device/xiaomi/sm6250-common
 
 # Vendor Tree
 git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll
-git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_sm6250-common.git -b 12.1  vendor/xiaomi/sm6250-common
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_sm6250-common.git -b 13  vendor/xiaomi/sm6250-common
 
 # Kernel Tree
-git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_sm6250.git kernel/xiaomi/sm6250
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_sm6250.git -b 13 kernel/xiaomi/sm6250
 
 # Device Extra
 git clone --depth=1 https://github.com/ArrowOS-Devices/android_device_xiaomi_extras.git -b arrow-12.0 device/xiaomi/extras
